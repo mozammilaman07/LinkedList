f5c1cdb92a409774c52469e62899c3a97b8940de
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-11-23 22:28:04 +0530

    created a class of Node

--- singlyLL.cpp
+++ singlyLL.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 // Creation of Linked List
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL:
+    }
+};
 
 int main()
 {
